a2436e55d23d850940fe54f5c3fa68c8d5bf3d7e
movelikeriver <noemail@local.com>
2015-05-30 22:42:50 -0700

    cycleday

---
+++ cycleday/cycleday.py
@@ -0,0 +1,32 @@
+import cgi
+import getopt
+import os
+import sys
+import traceback
+
+from google.appengine.api import users
+from google.appengine.ext import webapp
+from google.appengine.ext.webapp.util import run_wsgi_app
+from google.appengine.ext import db
+from google.appengine.ext.webapp import template
+
+print '-' * 80
+print __doc__
+print '-' * 80
+
+class MainPage(webapp.RequestHandler):
+    def get(self):
+        path = os.path.join(os.path.dirname(__file__), 'index.html')
+        self.response.out.write(template.render(path, {}))
+#		self.response.out.write(open(path).read())
+    
+
+application = webapp.WSGIApplication(
+                                     [('/', MainPage)],
+                                     debug=True)
+
+def main():
+    run_wsgi_app(application)
+
+if __name__ == "__main__":
+    main()
